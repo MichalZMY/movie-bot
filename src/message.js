3f9faab2c3f7704dff8fbcb65b5ad365702a7cdb
Nathan Grasset <nathan.grasset@recast.ai>
2017-09-18 18:15:53 +0200

    feat(core) improve step1 to send reply from the bot builder

--- src/message.js
+++ src/message.js
@@ -6,11 +6,30 @@ const constants = require('./constants');
 
 function onNewMessage(message) {
   const text = message.content;
+  const senderId = message.senderId;
 
   console.log('The bot received: ', text);
-  return message.reply([{ type: 'text', content: 'Hello world!' }])
+
+  return recastBot.request
+    .converseText(text, { conversationToken: senderId })
+    .then(function(conversation) {
+      if (!conversation.action) {
+        return message.reply({
+          type: 'text',
+          content: "I don't have an action for this sentence yet :(",
+        });
+      }
+
+      console.log('The conversation action is: ', conversation.action.slug);
+
+      conversation.replies.forEach(function(replyContent) {
+        message.addReply({ type: 'text', content: replyContent });
+      });
+
+      return message.reply();
+    })
     .catch(function(err) {
-      console.error('message::reply error: ', err);
+      console.error('Recast::request::converseText error: ', err);
     });
 }
 
